0a90efd649aabf4ef9f230a4b81713dc5215d1d2
Thomas Eitler <thomas.eitler@gmail.com>
2013-03-04 20:19:49 +0100

    start adding live parsing of klostats

--- module_klo.php
+++ module_klo.php
@@ -7,6 +7,7 @@ $mtg_counter = 0;
 
 function klo_init() {
 	klo_reset_counter();
+	klo_listen();
 }
 
 function klo_reset_counter() {
@@ -28,12 +29,36 @@ function klo_command($string, $target='', $private=0) {
 		case 'show':
 			irc_send_message("Klo ".$klo_counter." : ".$mtg_counter." Meetings", $target, $private);
 			break;
+		case 'k++':
 		case 'klo+1':
 			$klo_counter++;
+			irc_send_message("Klo ".$klo_counter." : ".$mtg_counter." Meetings", $target, $private);
 			break;
+		case 'm++':
 		case 'mtg+1':
 			$mtg_counter++;
+			irc_send_message("Klo ".$klo_counter." : ".$mtg_counter." Meetings", $target, $private);
 			break;
+	}
+}
+
+function klo_listen(){
+	$line = trim(fgets($res));
+
+	if (strpos($line, ' PRIVMSG '.IRC_CHANNEL.' ') !== false) {
+		echo "Received message...\n";
+		$sender = substr($line, 1, strpos($line, '!')-1);
+		echo "From: ".$sender."\n";
+		$msg = substr($line, strpos($line, ':', 2)+1);
+		echo "Message: ".$msg."\n";
+		if (strpos($msg, IRC_NICK) !== false) {
+			echo "I was mentioned\n";
+			if (substr($msg, 0, strlen(IRC_NICK)) == IRC_NICK) {
+				$msg = substr($msg, strpos($msg, ' ') + 1);
+				echo $msg;
+				#interpret_irc_message($sender, $msg, 0);
+			}
+		}
 	}
 }
 
