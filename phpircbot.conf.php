6751abf22f077935ca2bb10fc25d2ce8683a8ffd
steffmeister <software@steffmeister.at>
2013-03-04 22:04:42 +0100

    added config file

---
+++ phpircbot.conf.php
@@ -0,0 +1,19 @@
+<?php
+
+/* general settings */
+define('IRC_HOST', 'irc.quakenet.org');
+define('IRC_PORT', '6667');
+define('IRC_CHANNEL', '#mychannel');
+define('IRC_NICK', 'steff^bot');
+
+/* how many tries to connect */
+define('IRC_HOST_RECONNECT', 5);
+
+/* admin users (comma-separated) */
+define('IRC_ADMIN_USERS', 'steffmeister');
+
+/* auto load modules (comma-separated) */
+define('AUTOLOAD_MODULES', 'klo,magicball');
+
+
+?>
